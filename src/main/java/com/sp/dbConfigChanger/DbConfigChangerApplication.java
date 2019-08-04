50e31c6fe81de25d42283393fa1624c8bcaf9f41
soutam <soutampaul@gmail.com>
2019-08-04 09:12:21 +0530

    first commit

---
+++ src/main/java/com/sp/dbConfigChanger/DbConfigChangerApplication.java
@@ -0,0 +1,13 @@
+package com.sp.dbConfigChanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbConfigChangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbConfigChangerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sp/dbConfigChanger/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sp.dbConfigChanger;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DbConfigChangerApplication.class);
+	}
+
+}
